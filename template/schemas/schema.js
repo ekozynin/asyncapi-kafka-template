007c9c089cf322faf1ff5b1d829499e38ed7a6da
Eugen Kozynin <ekozynin@gmail.com>
2021-08-18 08:49:58 +1000

    code cleanup & improved debugging

--- template/schemas/schema.js
+++ template/schemas/schema.js
@@ -22,10 +22,9 @@ export default function({ asyncapi }) {
 }
 
 function createMessageCommand(messageName, message, schemaRegistry) {
-  console.log(`------------------->${  JSON.stringify(message)}`);
-
   const messagePayload = message.originalPayload();
   const messageNamespace = messagePayload.namespace;
+  const schemaType = 'AVRO'; // TODO
 
   return `import logging
 
@@ -36,19 +35,20 @@ from confluent_kafka.schema_registry import SchemaRegistryClient, Schema
 def register_schema(schema_registry_client, schema_name, schema_type, schema_payload, compatibility_level = 'FULL'):
     schema = Schema(schema_payload, schema_type=schema_type)
 
-    logging.debug('Registering schema {}'.format(schema_name))
     try:
         schemaVersion = schema_registry_client.register_schema(schema_name, schema)
-        logging.debug('Value schema for topic {} has been registered with the version {}'.format(schema_name, schemaVersion))
+        logging.info('Value schema for topic {} has been registered with the version {}'.format(schema_name, schemaVersion))
     except SchemaRegistryError as e:
         logging.error("Failed to register value schema for topic {}: {}".format(schema_name, e))
+        raise e
 
     logging.debug('Setting schema compatibility level to {} for topic {}'.format(compatibility_level, schema_name))
     try:
         resultCompatibilityLevel = schema_registry_client.set_compatibility(schema_name, compatibility_level)
-        logging.debug('Topic {} schema compatibility level is {}'.format(schema_name, resultCompatibilityLevel['compatibility']))
+        logging.info('Topic {} schema compatibility level is {}'.format(schema_name, resultCompatibilityLevel['compatibility']))
     except SchemaRegistryError as e:
         logging.error("Failed to set schema compatibility for schema {} to {} level: {}".format(schema_name, compatibility_level, e))
+        raise e
 ### ----- END register_topic_schema()
 
 ### ----- main()
@@ -56,8 +56,9 @@ def main():
     logging.debug('Creating SchemaRegistryClient with schemaRegistryUrl ${schemaRegistry}')
     schema_registry_client = SchemaRegistryClient({'url': '${schemaRegistry}'})
 
+    logging.info("-------------------------------------")
     logging.info('Registering schema ${messageNamespace}.${messageName}')
-    register_schema(schema_registry_client, '${messageNamespace}.${messageName}', 'AVRO', '${JSON.stringify(messagePayload)}')
+    register_schema(schema_registry_client, '${messageNamespace}.${messageName}', '${schemaType}', '${JSON.stringify(messagePayload)}')
 ### ----- END main()
 
   `;
--- template/topics/topic.js
+++ template/topics/topic.js
@@ -1,8 +1,6 @@
 import { File } from '@asyncapi/generator-react-sdk';
 
 export default function({ asyncapi }) {
-  const bootstrapServers = 'localhost:9092'; // TODO
-  const schemaRegistry = 'http://localhost:8081'; // TODO
   const channels = asyncapi.channels();
 
   const messagesFiles = [];
@@ -12,7 +10,7 @@ export default function({ asyncapi }) {
 
     messagesFiles.push(
       <File name={fileName}>
-        {createTopicCommand(channelName, channel, bootstrapServers, schemaRegistry)}
+        {createTopicCommand(channelName, channel)}
       </File>
     );
   });
@@ -20,7 +18,7 @@ export default function({ asyncapi }) {
   return messagesFiles;
 }
 
-function createTopicCommand(channelName, channel, bootstrapServers, schemaRegistry) {
+function createTopicCommand(channelName, channel) {
   const partitions = channel.bindings().kafka.partitions;
   const replicationFactor = channel.bindings().kafka.replicas;
   const topicConfigs = channel.bindings().kafka.configs;
@@ -28,64 +26,87 @@ function createTopicCommand(channelName, channel, bootstrapServers, schemaRegist
 
   return `import logging
 
-from confluent_kafka.admin import AdminClient, NewTopic
+from confluent_kafka.admin import AdminClient, NewTopic, ConfigResource
 from confluent_kafka.schema_registry import SchemaRegistryClient, Schema
+from confluent_kafka import KafkaException, KafkaError
 
 ### ----- create_new_topic()
-def create_new_topic(admin_client, channelName, partitions, replicationFactor, topicConfigs):
-    new_topics = [NewTopic(channelName, num_partitions=partitions, replication_factor=replicationFactor, config=topicConfigs)]
+def create_new_topic(admin_client, channel_name, partitions, replicationFactor, topic_configs):
+    # check if the topic already exists, pull the topic's definition, and alter the topic as required
+    topic_config_resource = ConfigResource(ConfigResource.Type.TOPIC, channel_name)
 
-    # Call create_topics to asynchronously create topics.
-    #  A dict of <topic,future> is returned.
-    fs = admin_client.create_topics(new_topics);
+    topic_config = None
+    try:
+        fs = admin_client.describe_configs([topic_config_resource])
+        topic_config = fs.get(topic_config_resource).result()
+    except KafkaException as e:
+        if e.args[0].code() == KafkaError.UNKNOWN_TOPIC_OR_PART:
+            logging.debug("Topic {} doesn't exists".format(channel_name))
+        else:
+            raise e
 
-    logging.debug('Creating new topic ${channelName}')
-    # Wait for each operation to finish.
-    for topic, f in fs.items():
+    if topic_config is None:
+        logging.debug('Creating new topic {}'.format(channel_name))
+        topic = NewTopic(channel_name, num_partitions=partitions, replication_factor=replicationFactor, config=topic_configs)
+
         try:
-            f.result()  # The result itself is None
-            logging.debug("Topic {} created".format(topic))
-        except Exception as e:
-            logging.error("Failed to create topic {}: {}".format(topic, e))
+            fs = admin_client.create_topics([topic])
+            fs.get(channel_name).result() # returns None
+            logging.info("Topic {} has been created.".format(channel_name))
+        except KafkaException as e:
+            if (e.args[0].code() == KafkaError.TOPIC_ALREADY_EXISTS):
+                logging.debug("Topic {} already exists".format(channel_name))
+            else:
+                raise e
+    else:
+        logging.info("Altering topic {} config".format(channel_name))
+
+        # TODO alter number of partitions if needed
+        # https://docs.confluent.io/platform/current/clients/confluent-kafka-python/html/index.html#confluent_kafka.admin.AdminClient.create_partitions
+
+        # TODO update topic replication factor if needed
+
+        # Merge values from existing config and new config
+        for config_entry in topic_config.values():
+            topic_config_resource.set_config(config_entry.name, config_entry.value)
+        for key in topic_configs:
+            logging.info("- Setting {}={}".format(key, topic_configs[key]))
+            topic_config_resource.set_config(key, topic_configs[key])
+
+        fs = admin_client.alter_configs([topic_config_resource])
+        fs.get(topic_config_resource).result()
 ### ----- END create_new_topic()
 
 ### ----- register_topic_schema()
 def register_topic_schema(schema_registry_client, channel_name, schema_type, schema_payload, compatibility_level = 'FULL'):
     schema = Schema(schema_payload, schema_type=schema_type)
 
-    logging.debug('Registering value schema for topic ${channelName}')
+    logging.debug('Registering value schema for topic {}'.format(channel_name))
     try:
         schemaVersion = schema_registry_client.register_schema(channel_name + '-value', schema)
-        logging.debug('Value schema for topic {} has been registered with the version {}'.format(channel_name, schemaVersion))
+        logging.info('Value schema for topic {} has been registered with the version {}'.format(channel_name, schemaVersion))
     except SchemaRegistryError as e:
         logging.error("Failed to register value schema for topic {}: {}".format(channel_name, e))
+        raise e
 
     logging.debug('Setting schema compatibility level to {} for topic {}'.format(compatibility_level, channel_name))
     try:
         resultCompatibilityLevel = schema_registry_client.set_compatibility(channel_name.join("-value"), compatibility_level)
-        logging.debug('Topic ${channelName} schema compatibility level is ' + resultCompatibilityLevel['compatibility'])
+        logging.info('Topic {} schema compatibility level is {}'.format(channel_name, resultCompatibilityLevel['compatibility']))
     except SchemaRegistryError as e:
         logging.error("Failed to set value schema compatibility for topic {} to {} level: {}".format(channel_name, compatibility_level, e))
+        raise e
 ### ----- END register_topic_schema()
 
 ### ----- main()
-def main():
-    logging.debug('Creating AdminClient with bootstrap.servers ${bootstrapServers}')
-    admin_client = AdminClient({'bootstrap.servers': '${bootstrapServers}'})
-
-    logging.debug('Creating SchemaRegistryClient with schemaRegistryUrl ${schemaRegistry}')
-    schema_registry_client = SchemaRegistryClient({'url': '${schemaRegistry}'})
-
-    # TODO check if the topic already exists, pull the topic's definition, and alter topic if required
-
-    # create new topic
-    logging.info('Creating new topic ${channelName}')
+def main(admin_client, schema_registry_client):
+    logging.info("-------------------------------------")
+    logging.info("Creating/updating topic '${channelName}'")
     create_new_topic(admin_client, '${channelName}', ${partitions}, ${replicationFactor}, ${JSON.stringify(topicConfigs)})
 
-    # register schema for the new topic
+    # register schema for the topic
     if 'true' == '${valueSchema !== undefined}':
-        logging.info('Registering value schema for topic ${channelName}')
-        register_topic_schema(schema_registry_client, '${channelName}', 'AVRO', '${JSON.stringify(valueSchema)}')
+        register_topic_schema(schema_registry_client, "${channelName}", "AVRO", '${JSON.stringify(valueSchema)}')
 ### ----- END main()
 
   `;
