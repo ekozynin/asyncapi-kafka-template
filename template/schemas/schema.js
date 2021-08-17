43e1de37fefc3c2b52e897a31553bfced2dd5dda
Eugen Kozynin <ekozynin@gmail.com>
2021-08-17 13:00:51 +1000

    altering topic config is working

--- template/schemas/schema.js
+++ template/schemas/schema.js
@@ -24,6 +24,7 @@ export default function({ asyncapi }) {
 function createMessageCommand(messageName, message, schemaRegistry) {
   const messagePayload = message.originalPayload();
   const messageNamespace = messagePayload.namespace;
+  const schemaType = 'AVRO' // TODO
 
   return `import logging
 
@@ -34,19 +35,20 @@ from confluent_kafka.schema_registry import SchemaRegistryClient, Schema
 def register_schema(schema_registry_client, schema_name, schema_type, schema_payload, compatibility_level = 'FULL'):
     schema = Schema(schema_payload, schema_type=schema_type)
 
-    logging.debug('Registering schema {}'.format(schema_name))
     try:
         schemaVersion = schema_registry_client.register_schema(schema_name, schema)
-        logging.debug('Value schema for topic {} has been registered with the version {}'.format(schema_name, schemaVersion))
+        logging.info('Value schema for topic {} has been registered with the version {}'.format(schema_name, schemaVersion))
     except SchemaRegistryError as e:
         logging.error("Failed to register value schema for topic {}: {}".format(schema_name, e))
+        raise e
 
     logging.debug('Setting schema compatibility level to {} for topic {}'.format(compatibility_level, schema_name))
     try:
         resultCompatibilityLevel = schema_registry_client.set_compatibility(schema_name, compatibility_level)
-        logging.debug('Topic {} schema compatibility level is {}'.format(schema_name, resultCompatibilityLevel['compatibility']))
+        logging.info('Topic {} schema compatibility level is {}'.format(schema_name, resultCompatibilityLevel['compatibility']))
     except SchemaRegistryError as e:
         logging.error("Failed to set schema compatibility for schema {} to {} level: {}".format(schema_name, compatibility_level, e))
+        raise e
 ### ----- END register_topic_schema()
 
 ### ----- main()
@@ -56,7 +58,7 @@ def main():
 
     logging.info("-------------------------------------")
     logging.info('Registering schema ${messageNamespace}.${messageName}')
-    register_schema(schema_registry_client, '${messageNamespace}.${messageName}', 'AVRO', '${JSON.stringify(messagePayload)}')
+    register_schema(schema_registry_client, '${messageNamespace}.${messageName}', '${schemaType}', '${JSON.stringify(messagePayload)}')
 ### ----- END main()
 
   `;
